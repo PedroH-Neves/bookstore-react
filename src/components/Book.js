af0d7e2fd764c7b0dc298d826d51636d750a5d29
PedroH-Neves <PedroHDN710@gmail.com>
2023-04-08 18:53:14 -0400

    change book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Book = () => (
-  <div>
-    <h1>
-      Harry potter by JK
-      <button type="button">Delete</button>
-    </h1>
-    <form>
-      <input placeholder="add title" />
-      <input placeholder="add author" />
-    </form>
-  </div>
-);
+function BookList() {
+  const [books, setBooks] = useState([]);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
 
-export default Book;
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBook = { title, author };
+    setBooks([...books, newBook]);
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div>
+      <h2>Book List</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={null}>
+            {book.title}
+            by
+            {book.author}
+          </li>
+        ))}
+      </ul>
+
+      <h2>Add a Book</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <input id="title" type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+        </div>
+        <div>
+          <input id="author" type="text" value={author} onChange={(event) => setAuthor(event.target.value)} />
+        </div>
+        <button type="submit">Add new book</button>
+      </form>
+    </div>
+  );
+}
+
+export default BookList;
