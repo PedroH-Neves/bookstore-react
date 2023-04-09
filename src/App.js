db9b6e5bcd065b1e0b75738f13f47c0c6a3c5f0b
PedroH-Neves <PedroHDN710@gmail.com>
2023-04-08 22:23:29 -0400

    change components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import
+{
+  BrowserRouter as Router, Routes, Route, Link,
+}
+  from 'react-router-dom';
 import './App.css';
-import Categories from './pages/Categories';
+import Categories from './components/Categories';
 import Book from './components/Book';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Book />} />
-        <Route path="/Categories" element={<Categories />} />
-      </Routes>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/categories">Categories</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Book />} />
+          <Route path="/Categories" element={<Categories />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
